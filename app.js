03e86f4b5d5466d4933c6b6407b83e579cec0324
Daniel Gunnlaugsson <daniel.thor.gunnlaugsson@gmail.com>
2018-05-17 21:45:47 +0200

    db connection with mongoose

--- app.js
+++ app.js
@@ -5,15 +5,28 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const passport = require('passport');
 const mongoose = require('mongoose');
+const config = require('./config/database');
+
+// Connect to DB
+mongoose.connect(config.database);
+
+mongoose.connection.on('connected', () => {
+    console.log('Connected to database ' + config.database);
+});
+
+mongoose.connection.on('error', (err) => {
+    console.log('Database error: ' + err);
+});
 
 const app = express();
-
 const users = require('./routes/users');
-
 const port = 1234;
 
 // Cors Middleware
 app.use(cors());
+
+// Set static folder for client side files
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Body Parser Middleware
 app.use(bodyParser.json());
